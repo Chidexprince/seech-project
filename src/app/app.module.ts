add6f8ef3ed6b1a8c0cdf1bc70a0d73234813c38
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-11-09 00:35:16 +0100

    Setting up strcture and component styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentsComponent } from './components/payments/payments.component';
+import { PaymentListComponent } from './components/payment-list/payment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentsComponent,
+    PaymentListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/payment.ts
@@ -0,0 +1,6 @@
+export class Payment {
+  public amount: number;
+  public recipient: string;
+  public description: string;
+  public date = new Date();
+}
